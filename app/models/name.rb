1d48a78ecd3cc09119b67661df1be71ef4f18e78
Steven Fernandez <sfernandez0201@gmail.com>
2024-10-23 15:23:34 -0400

    switch to falcon and switch from turbo refresh to other turbo action

--- app/models/name.rb
+++ app/models/name.rb
@@ -3,6 +3,6 @@ class Name < ApplicationRecord
 
   private
   def broadcast_turbo_refresh
-    Turbo::StreamsChannel.broadcast_refresh_later_to :names
+    Turbo::StreamsChannel.broadcast_update_later_to(:names, target: "name_#{self.id}", html: "<span id='name--points' data-points-target='value' class='text-game-yellow text-3xl'>#{self.points}</span>")
   end
 end
